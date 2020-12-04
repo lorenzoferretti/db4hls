4b52ad3eec8892c25536886122c41a8137b8eea8
lorenzoferretti <ferrelo@usi.ch>
2020-12-04 12:05:31 -0500

    updated files

--- db4hls_config.sh
+++ db4hls_config.sh
@@ -5,6 +5,7 @@ mkdir tcl_scripts
 mkdir venv
 
 # Create virtual environment and install required modules
-python3.7 -m venv venv
+virtualenv -p python3 ./venv
+#python3.7 -m venv venv
 source ./venv/bin/activate
 ./venv/bin/pip install -r requirements.txt 
--- src/main.py
+++ src/main.py
@@ -17,26 +17,33 @@ synthesis_tool = "vivado_hls"
 technology = "xczu9eg-ffvb1156-2-e"
 
 # Specify the path of the source files
-src_files_path = ""
+# Assuming that the Machsuite benchmark folder is in the project root level
+# src_files_path = "../MachSuite/backprop/backprop/"
+src_files_path = "../../DSE_exhaustive/benchmark/MachSuite/backprop/backprop/"
 
 # List of the source file names
-src_files = ["",""]
+src_files = ["backprop.c","backprop.h"]
 
 # Testbench file if cosimulation is requried
 testbench = ""
 
+# Specify the design
+design = "backprop"
+
 # Specify top_module (name of the function to be synthesised)
-synth = synthesis.Synthesiser(synthesis_tool)
+top_module = "get_delta_matrix_weights1"
 
 # Specify destination folder of the synthesis
-destination_folder_path = ""
+destination_folder_path = "../syn/"
 
 # Specify destination folder of the hls file generated
-hls_scripts_folder_path = "../"
+hls_scripts_folder_path = "../tcl_scripts/"
 
 # Specify the configuration space descriptor file
 csd_file = "../csd_files/last_step_scan_radix_sort.sweep"
 
+# Specify the maximum amount of time allowed per synthesis
+max_synthesis_time = 60
 
 ### Start processing
 
@@ -44,10 +51,18 @@ csd_file = "../csd_files/last_step_scan_radix_sort.sweep"
 synth = synthesis.Synthesiser(synthesis_tool)
 
 # Set synthesised project data and board spec
-synth.set_project(src_files_path, src_files, testbench, top_module, implementation,
+synth.set_project(src_files_path, src_files, testbench, top_module, design,
                   destination_folder_path, hls_scripts_folder_path)
 synth.set_board_specs(technology)
 
 # Process the configuration space descriptor
-csd = synth.set_exploration_knobs(csd_file)
-new_feature_sets = ds['Knobs_values']
+configuration_space_descriptor = synth.set_exploration_knobs(csd_file)
+knob_value_sets = configuration_space_descriptor['Knobs_values']
+
+print(knob_value_sets)
+
+# Generate configurations from configuration space descriptor
+configs = synth.exhaustive_configs()
+print(configs)
+
+VivadoHLS.create_directive_script(configs)
